b6fd76f7e362756f3daf3693ea8fcf691509146e
Артём Уханов <gartemonka@mail.ru>
2025-01-25 16:37:56 +0300

    fix

--- stat/stat-dto/src/main/java/ewm/ViewStats.java
+++ stat/stat-dto/src/main/java/ewm/ViewStats.java
@@ -1,16 +1,17 @@
 package ewm;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
+@Data
 @NoArgsConstructor
 public class ViewStats {
     private String app;
     private String uri;
     private Long hits;
+
+    public ViewStats(String app, String uri, Long hits) {
+        this.app = app;
+        this.uri = uri;
+        this.hits = hits;
+    }
 }
